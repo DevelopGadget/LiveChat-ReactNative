63dd6e71244cbc8d40e0fe057c98d0f5bf7bfbe3
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-08-23 19:39:09 -0500

    Login

--- Src/Components/Login.js
+++ Src/Components/Login.js
@@ -1,30 +1,45 @@
 import React from 'react';
-import { Container, Form, Item, Input, Label, Content, Icon } from 'native-base';
+import { Container, Form, Item, Input, Label, Icon, Button, Text } from 'native-base';
 import Estilos from '../Css/Estilos';
 import { LinearGradient } from 'expo';
+import { Image } from 'react-native';
+import { Grid, Row } from 'react-native-easy-grid';
+import { SimpleAnimation } from 'react-native-simple-animations';
 
 export default class Login extends React.Component {
     render() {
         return (
             <Container>
                 <LinearGradient colors={['#800080', '#000']} start={[0, 1]} end={[1, 0]} style={Estilos.Pantalla}>
-                    <Content contentContainerStyle={[Estilos.Content, Estilos.CenterFlex]} padder>
-                        <Form>
-                            <Item floatingLabel>
-                                <Icon name='email' style={Estilos.Color} type='Entypo' />
-                                <Label style={Estilos.Color}>Email</Label>
-                                <Input style={Estilos.Color} />
-                            </Item>
-                            <Item floatingLabel>
-                                <Icon name='vpn-key' style={Estilos.Color} type='MaterialIcons' />
-                                <Label style={Estilos.Color}>Password</Label>
-                                <Input style={Estilos.Color} />
-                            </Item>
-                        </Form>
-                    </Content>
+                    <SimpleAnimation style={{ flex: 1 }} fade delay={500} duration={1000} staticType='bounce' movementType='slide' direction='left'>
+                        <Grid style={Estilos.Content}>
+                            <Row size={3} style={Estilos.CenterFlex}>
+                                <Image source={require('../../assets/icon.png')} style={Estilos.Imagen} resizeMode='contain' />
+                            </Row>
+                            <Row size={1}>
+                                <Form style={Estilos.Form}>
+                                    <Item floatingLabel>
+                                        <Icon name='email' style={Estilos.Color} type='Entypo' />
+                                        <Label style={Estilos.Color}>Email</Label>
+                                        <Input style={Estilos.Color} />
+                                    </Item>
+                                    <Item floatingLabel>
+                                        <Icon name='vpn-key' style={Estilos.Color} type='MaterialIcons' />
+                                        <Label style={Estilos.Color}>Password</Label>
+                                        <Input style={Estilos.Color} />
+                                    </Item>
+                                </Form>
+                            </Row>
+                            <Row size={1} style={Estilos.CenterFlex}>
+                                <Button iconLeft style={Estilos.Boton}>
+                                    <Icon name='login' type='Entypo' />
+                                    <Text>Entrar</Text>
+                                </Button>
+                            </Row>
+                        </Grid>
+                    </SimpleAnimation>
                 </LinearGradient>
             </Container>
         );
     }
-
 }
--- Src/Css/Estilos.js
+++ Src/Css/Estilos.js
@@ -6,12 +6,26 @@ export default Estilo = StyleSheet.create({
         height: '100%'
     },
     Content: {
-        flex: 1
+        flex: 1,
     },
     CenterFlex: {
-        justifyContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     Color: {
         color: 'violet'
+    },
+    Imagen: {
+        width: '70%',
+        height: '70%',
+        alignItems: 'center',
+        flex: 1
+    },
+    Form: {
+        flex: 1,
+        justifyContent: 'flex-start'
+    },
+    Boton: {
+        backgroundColor: 'violet'
     }
 });
